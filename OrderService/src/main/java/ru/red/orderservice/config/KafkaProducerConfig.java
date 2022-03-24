f10ebd4749242811ab2da6668a0f8fa3cfeccb75
ReDestroyDeR <daniil.schroder@gmail.com>
2022-03-24 08:34:55 +0300

    Fix typo

--- OrderService/src/main/java/ru/red/orderservice/config/KafkaProducerConfig.java
+++ OrderService/src/main/java/ru/red/orderservice/config/KafkaProducerConfig.java
@@ -1,12 +1,16 @@
 package ru.red.orderservice.config;
 
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.kafka.sender.SenderOptions;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import streamprocessing.avro.KeyOrderManipulation;
+import streamprocessing.avro.ValueOrderManipulation;
 
 import java.util.HashMap;
 
@@ -19,13 +23,18 @@ public class KafkaProducerConfig {
     private String schemaRegistryUrl;
 
 
-    @Bean
-    public SenderOptions<?, ?> senderOptions() {
+    @Bean("order-manipulation-producer-factory")
+    public ProducerFactory<KeyOrderManipulation, ValueOrderManipulation> orderProducerFactory() {
         var props = new HashMap<String, Object>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-        return SenderOptions.create(props);
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean("order-manipulation-kafka-template")
+    public KafkaTemplate<KeyOrderManipulation, ValueOrderManipulation> orderKafkaTemplate() {
+        return new KafkaTemplate<>(orderProducerFactory());
     }
 }
--- OrderService/src/main/java/ru/red/orderservice/producer/OrderProducer.java
+++ OrderService/src/main/java/ru/red/orderservice/producer/OrderProducer.java
@@ -1,44 +1,30 @@
 package ru.red.orderservice.producer;
 
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.kafka.sender.KafkaSender;
-import reactor.kafka.sender.SenderOptions;
-import reactor.kafka.sender.SenderRecord;
-import reactor.kafka.sender.SenderResult;
+import reactor.core.publisher.Mono;
 import ru.red.orderservice.domain.Order;
 import streamprocessing.avro.KeyOrderManipulation;
 import streamprocessing.avro.ValueOrderManipulation;
 
-import javax.annotation.PreDestroy;
-
 @Component
 public class OrderProducer {
-    private final KafkaSender<KeyOrderManipulation, ValueOrderManipulation> producer;
+    private final KafkaTemplate<KeyOrderManipulation, ValueOrderManipulation> kafka;
 
     @Autowired
-    public OrderProducer(SenderOptions<KeyOrderManipulation, ValueOrderManipulation> senderOptions) {
-        this.producer = KafkaSender.create(senderOptions);
+    public OrderProducer(KafkaTemplate<KeyOrderManipulation, ValueOrderManipulation> kafka) {
+        this.kafka = kafka;
     }
 
-    public Flux<SenderResult<String>> sendCreatedMessage(Flux<Order> order) {
-        return producer.send(order.map(o ->
-                        SenderRecord.create(
-                                new ProducerRecord<KeyOrderManipulation, ValueOrderManipulation>
-                                        ("order-manipulation",
-                                                new KeyOrderManipulation(o.getUserAddress()),
-                                                new ValueOrderManipulation(o.getId(), o.getTotalPrice())
-                                        ),
-                                "Created " + o.getId()
-                        )
-                )
-        );
-    }
-
-    @PreDestroy
-    public void closeProducer() {
-        producer.close();
+    public Flux<SendResult<KeyOrderManipulation, ValueOrderManipulation>> sendCreatedMessage(Flux<Order> order) {
+        return order.flatMap(o -> Mono.fromFuture(
+                kafka.send("order-manipulation",
+                                new KeyOrderManipulation(o.getUserAddress()),
+                                new ValueOrderManipulation(o.getId(), o.getTotalPrice()))
+                        .completable()
+        ));
     }
 }
