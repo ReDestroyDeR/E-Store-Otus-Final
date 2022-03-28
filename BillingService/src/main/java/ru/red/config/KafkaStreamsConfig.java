a42abc6ca663bef4efa88e54c7b0e65cb09f03ae
ReDestroyDeR <daniil.schroder@gmail.com>
2022-03-28 22:18:14 +0300

    Billing order processing

--- BillingService/src/main/java/ru/red/config/KafkaStreamsConfig.java
+++ BillingService/src/main/java/ru/red/config/KafkaStreamsConfig.java
@@ -1,17 +1,19 @@
 package ru.red.config;
 
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.subject.TopicRecordNameStrategy;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +21,7 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 @EnableKafkaStreams
+@Profile("kafka-streams")
 public class KafkaStreamsConfig {
 
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
@@ -32,6 +35,7 @@ public class KafkaStreamsConfig {
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(AbstractKafkaSchemaSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY, TopicRecordNameStrategy.class);
         properties.put("schema.registry.url", schemaRegistryUrl);
 
         return new KafkaStreamsConfiguration(properties);
@@ -39,6 +43,7 @@ public class KafkaStreamsConfig {
 
     @Bean("serde-config")
     public Map<String, String> serdeConfig(@Value("${kafka.schemaRegistryUrl}") String schemaRegistryUrl) {
-        return Collections.singletonMap("schema.registry.url", schemaRegistryUrl);
+        return Map.of("schema.registry.url", schemaRegistryUrl,
+                AbstractKafkaSchemaSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY, TopicRecordNameStrategy.class.getName());
     }
 }
--- BillingService/src/main/java/ru/red/streams/NewOrderProcessor.java
+++ BillingService/src/main/java/ru/red/streams/NewOrderProcessor.java
@@ -5,31 +5,38 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Branched;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.util.Pair;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import ru.red.billing.avro.OrderAcknowledged;
+import ru.red.billing.avro.OrderAcknowledgmentKey;
+import ru.red.billing.avro.OrderNotAcknowledged;
+import ru.red.domain.UserBilling;
+import ru.red.order.avro.OrderCancelled;
+import ru.red.order.avro.OrderManipulationKey;
+import ru.red.order.avro.OrderPlaced;
 import ru.red.service.UserBillingService;
-import streamprocessing.avro.KeyOrderAcknowledgment;
-import streamprocessing.avro.KeyOrderManipulation;
-import streamprocessing.avro.OrderAckStatus;
-import streamprocessing.avro.ValueOrderAcknowledgment;
-import streamprocessing.avro.ValueOrderManipulation;
 
 import java.util.Map;
 
 @Log4j2
 @Component
+@Profile("kafka-streams")
 public class NewOrderProcessor {
-    private static final Serde<KeyOrderManipulation> KEY_ORDER_MANIPULATION_SERDE = new SpecificAvroSerde<>();
-    private static final Serde<ValueOrderManipulation> VALUE_ORDER_MANIPULATION_SERDE = new SpecificAvroSerde<>();
+    private static final Serde<OrderManipulationKey> ORDER_MANIPULATION_KEY_SERDE = new SpecificAvroSerde<>();
+    private static final Serde<OrderPlaced> ORDER_PLACED_SERDE = new SpecificAvroSerde<>();
+    private static final Serde<OrderCancelled> ORDER_CANCELLED_SERDE = new SpecificAvroSerde<>();
 
-    private static final Serde<KeyOrderAcknowledgment> KEY_ORDER_ACKNOWLEDGMENT_SERDE = new SpecificAvroSerde<>();
-    private static final Serde<ValueOrderAcknowledgment> VALUE_ORDER_ACKNOWLEDGMENT_SERDE = new SpecificAvroSerde<>();
+    private static final Serde<OrderAcknowledgmentKey> ORDER_ACKNOWLEDGMENT_KEY_SERDE = new SpecificAvroSerde<>();
+    private static final Serde<OrderAcknowledged> ORDER_ACKNOWLEDGED_SERDE = new SpecificAvroSerde<>();
+    private static final Serde<OrderNotAcknowledged> ORDER_NOT_ACKNOWLEDGED_SERDE = new SpecificAvroSerde<>();
+
+    private static final String ORDER_ACKNOWLEDGMENT_TOPIC_NAME = "order-acknowledgment";
 
     private final UserBillingService billingService;
 
@@ -37,64 +44,56 @@ public class NewOrderProcessor {
     public NewOrderProcessor(UserBillingService billingService,
                              @Qualifier("serde-config") Map<String, String> serdeConfig) {
         this.billingService = billingService;
-        KEY_ORDER_MANIPULATION_SERDE.configure(serdeConfig, true);
-        VALUE_ORDER_MANIPULATION_SERDE.configure(serdeConfig, false);
+        ORDER_MANIPULATION_KEY_SERDE.configure(serdeConfig, true);
+        ORDER_PLACED_SERDE.configure(serdeConfig, false);
+        ORDER_CANCELLED_SERDE.configure(serdeConfig, false);
 
-        KEY_ORDER_ACKNOWLEDGMENT_SERDE.configure(serdeConfig, true);
-        VALUE_ORDER_ACKNOWLEDGMENT_SERDE.configure(serdeConfig, false);
+        ORDER_ACKNOWLEDGMENT_KEY_SERDE.configure(serdeConfig, true);
+        ORDER_ACKNOWLEDGED_SERDE.configure(serdeConfig, false);
+        ORDER_NOT_ACKNOWLEDGED_SERDE.configure(serdeConfig, false);
     }
 
     @Autowired
     void buildPipeline(StreamsBuilder builder) {
-        builder.stream("order-manipulation",
-                        Consumed.with(KEY_ORDER_MANIPULATION_SERDE, VALUE_ORDER_MANIPULATION_SERDE))
-                .map((key, value) -> {
-                    var email = key.getUserAddress().toString();
-                    var orderId = value.getOrderId().toString();
-                    var totalPrice = value.getOrderTotalPrice();
-
-                    var ackStatusBalancePairOptional = billingService.findByEmail(email)
-                            .publishOn(Schedulers.boundedElastic())
-                            .map(billing -> {
-                                if (billing.getBalance() - totalPrice < 0) {
-                                    return Pair.of(OrderAckStatus.NACK, billing.getBalance());
-                                }
-
-                                billing = billingService.removeFundsFromUser(email, totalPrice).block();
-                                assert billing != null;
-                                return Pair.of(OrderAckStatus.ACK, billing.getBalance());
-                            })
-                            .doOnNext(pair -> log.info(
-                                    "Order {} {} total price {} {}",
-                                    orderId, email, totalPrice, pair.getFirst()))
-                            .onErrorResume(e -> {
-                                log.warn(
-                                        "Accepted order {} to non existent billing {}",
-                                        orderId,
-                                        email
-                                );
-
-                                return Mono.empty();
-                            })
-                            .blockOptional();
-
-                    if (ackStatusBalancePairOptional.isEmpty())
-                        return KeyValue.pair(null, null);
-
-                    var ackStatusBalancePair = ackStatusBalancePairOptional.get();
-
-                    var outputKey = new KeyOrderAcknowledgment();
-                    var outputValue = new ValueOrderAcknowledgment();
-                    outputKey.setUserAddress(email);
-                    outputValue.setOrderId(orderId);
-                    outputValue.setOrderTotalPrice(totalPrice);
-                    outputValue.setEvent(ackStatusBalancePair.getFirst());
-                    outputValue.setUserBalance(ackStatusBalancePair.getSecond());
-
-                    return KeyValue.pair(outputKey, outputValue);
-                })
-                .filterNot(((key, value) -> key == null && value == null))
-                .to("order-acknowledgment",
-                        Produced.with(KEY_ORDER_ACKNOWLEDGMENT_SERDE, VALUE_ORDER_ACKNOWLEDGMENT_SERDE));
+        builder.stream("valid-orders",
+                        Consumed.with(ORDER_MANIPULATION_KEY_SERDE, ORDER_PLACED_SERDE))
+                .split()
+                .branch((key, value) ->
+                                Boolean.TRUE.equals(billingService.findByEmail(key.getEmail().toString())
+                                        .publishOn(Schedulers.boundedElastic())
+                                        .map(billing -> billing.getBalance() - value.getTotalPrice() >= 0)
+                                        .defaultIfEmpty(false)
+                                        .onErrorReturn(false)
+                                        .block()),
+                        Branched.withConsumer(stream -> stream
+                                        .map((key, value) -> {
+                                            var balance =
+                                                    billingService.removeFundsFromUser(key.getEmail().toString(),
+                                                                    value.getTotalPrice())
+                                                            .publishOn(Schedulers.boundedElastic())
+                                                            .map(UserBilling::getBalance)
+                                                            .block();
+                                            return new KeyValue<>(
+                                                    new OrderAcknowledgmentKey(key.getOrderId(), key.getEmail()),
+                                                    new OrderAcknowledged(value.getTotalPrice(), balance, value.getItems()));
+                                        })
+                                        .to(ORDER_ACKNOWLEDGMENT_TOPIC_NAME,
+                                                Produced.with(ORDER_ACKNOWLEDGMENT_KEY_SERDE, ORDER_ACKNOWLEDGED_SERDE)),
+                                "acknowledged-orders"))
+                .defaultBranch(Branched.withConsumer(stream -> stream
+                                .map((key, value) -> {
+                                    // Code duplication from predicate
+                                    var balance = billingService.findByEmail(key.getEmail().toString())
+                                            .publishOn(Schedulers.boundedElastic())
+                                            .map(UserBilling::getBalance)
+                                            .onErrorReturn(0)
+                                            .block();
+                                    return new KeyValue<>(
+                                            new OrderAcknowledgmentKey(key.getOrderId(), key.getEmail()),
+                                            new OrderNotAcknowledged(value.getTotalPrice(), balance, value.getItems()));
+                                })
+                                .to(ORDER_ACKNOWLEDGMENT_TOPIC_NAME,
+                                        Produced.with(ORDER_ACKNOWLEDGMENT_KEY_SERDE, ORDER_NOT_ACKNOWLEDGED_SERDE)),
+                        "not-acknowledged-orders"));
     }
 }
